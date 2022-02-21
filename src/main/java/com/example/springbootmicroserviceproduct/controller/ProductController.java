e2609c5bc8a2139500a4a06b757f0c87038d0ad6
Tolunay Toprakkaya <tolunaytoprakkaya@gmail.com>
2022-02-21 19:34:16 +0300

    ProductController base url changed

--- src/main/java/com/example/springbootmicroserviceproduct/controller/ProductController.java
+++ src/main/java/com/example/springbootmicroserviceproduct/controller/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/product")
+@RequestMapping("/api/product")
 public class ProductController {
 
     private final IProductService productService;
